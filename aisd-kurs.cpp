3d694e1e6b01e88e08afe63736a89337da2ef8e2
Kirill <kiril2306@gmail.com>
2021-11-30 11:35:23 +0300

    i hope it works

--- aisd-kurs.cpp
+++ aisd-kurs.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
+    
 class DSU {
     int* parent;
     int* rank;
@@ -46,7 +46,14 @@ public:
         edgelist.push_back({ w,x,y });
     }
     int kruskals_mst() {
-        sort(edgelist.begin(), edgelist.end());
+
+        for (int i = 0; i < edgelist.size(); i++) {
+            for (int j = i; j < (edgelist.size()); j++) {
+                if (edgelist[i] > edgelist[j]) {
+                    swap(edgelist[i], edgelist[j]);
+                }
+            }
+        }
 
         DSU s(V);
         int ans = 0;
@@ -56,6 +63,7 @@ public:
             int y = edge[2];
 
             if (s.find(x) != s.find(y)) {
+                cout << x << " - " << y << endl;
                 s.unite(x, y);
                 ans += w;
             }
@@ -64,15 +72,62 @@ public:
     }
 };
 
+struct input {
+    int x;
+    int y;
+    int w;
+};
+
 int main()
 {
-    Graph g(4);
-    g.addEdge(0, 1, 1);// x, y, weight
-    g.addEdge(1, 3, 3);
-    g.addEdge(3, 2, 4);
-    g.addEdge(2, 0, 2);
-    g.addEdge(0, 3, 2);
-    g.addEdge(1, 2, 2);
+    
+        // x, y, weight
+    /*
+    input z[51];
+    int c=0,x,y,w;
+    while (x != '\0'){
+      cin>>x>>y>>w;
+      if (x == '\0') break;
+      c++;
+
+    }
+
+    */
+    Graph g(20);
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 11, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+    g.addEdge(8, 9, 4);
+    g.addEdge(9, 10, 7);
+    g.addEdge(10, 2, 9);//17
+    g.addEdge(2, 11, 3);
+    g.addEdge(11, 7, 1);//19
+    g.addEdge(10, 11, 4);
+
+    /*g.addEdge(10, 17, 8);
+    g.addEdge(11, 12, 8);
+    g.addEdge(11, 17, 11);
+    g.addEdge(12, 13, 7);
+    g.addEdge(12, 18, 2);
+    g.addEdge(12, 15, 4);
+    g.addEdge(13, 14, 9);
+    g.addEdge(13, 15, 14);
+    g.addEdge(14, 15, 10);
+    g.addEdge(15, 16, 2);
+    g.addEdge(16, 17, 1);
+    g.addEdge(16, 18, 6);
+    g.addEdge(17, 18, 7);*/
 
     cout << g.kruskals_mst();
 }
