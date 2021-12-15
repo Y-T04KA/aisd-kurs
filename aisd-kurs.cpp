57c64591eb1448f783ea365131502c6f7c6fae2f
Kirill <kiril2306@gmail.com>
2021-12-15 21:19:11 +0300

    switch from windows-1251 to utf-8

--- aisd-kurs.cpp
+++ aisd-kurs.cpp
@@ -3,31 +3,31 @@
 #include <fstream>
 
 using namespace std;
-    
+
 class DisjointSet {
     int* parent;
     int* size;
 public:
-    DisjointSet(int n) {//тут мы инициализируем пустое подмножество на число вершин
-        parent = new int[n];//тут какая вершина к кому относится
+    DisjointSet(int n) {//С‚СѓС‚ РјС‹ РёРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј РїСѓСЃС‚РѕРµ РїРѕРґРјРЅРѕР¶РµСЃС‚РІРѕ РЅР° С‡РёСЃР»Рѕ РІРµСЂС€РёРЅ
+        parent = new int[n];//С‚СѓС‚ РєР°РєР°СЏ РІРµСЂС€РёРЅР° Рє РєРѕРјСѓ РѕС‚РЅРѕСЃРёС‚СЃСЏ
         size = new int[n];
         for (int i = 0; i < n; i++) {
-            parent[i] = -1;//изначально каждая вершина относится сама к себе т.е является началом своего подможества
-            size[i] = 1;//размер подмножества, по умолчанию 1
+            parent[i] = -1;//РёР·РЅР°С‡Р°Р»СЊРЅРѕ РєР°Р¶РґР°СЏ РІРµСЂС€РёРЅР° РѕС‚РЅРѕСЃРёС‚СЃСЏ СЃР°РјР° Рє СЃРµР±Рµ С‚.Рµ СЏРІР»СЏРµС‚СЃСЏ РЅР°С‡Р°Р»РѕРј СЃРІРѕРµРіРѕ РїРѕРґРјРѕР¶РµСЃС‚РІР°
+            size[i] = 1;//СЂР°Р·РјРµСЂ РїРѕРґРјРЅРѕР¶РµСЃС‚РІР°, РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ 1
         }
     }
-    int find(int i) {//поиск начала подмножества, в котором находится i
+    int find(int i) {//РїРѕРёСЃРє РЅР°С‡Р°Р»Р° РїРѕРґРјРЅРѕР¶РµСЃС‚РІР°, РІ РєРѕС‚РѕСЂРѕРј РЅР°С…РѕРґРёС‚СЃСЏ i
         if (parent[i] == -1) return i;
         return parent[i] = find(parent[i]);
     }
-    void unite(int x, int y) {//склеивает подмножества
+    void unite(int x, int y) {//СЃРєР»РµРёРІР°РµС‚ РїРѕРґРјРЅРѕР¶РµСЃС‚РІР°
         int s1 = find(x), s2 = find(y);
         if (s1 != s2) {
-            if (size[s1] < size[s2]){//если подмножество s2 больше, то s1 присоединяется к нему
+            if (size[s1] < size[s2]) {//РµСЃР»Рё РїРѕРґРјРЅРѕР¶РµСЃС‚РІРѕ s2 Р±РѕР»СЊС€Рµ, С‚Рѕ s1 РїСЂРёСЃРѕРµРґРёРЅСЏРµС‚СЃСЏ Рє РЅРµРјСѓ
                 parent[s1] = s2;
                 size[s2] += size[s1];
             }
-            else {//и наоборот
+            else {//Рё РЅР°РѕР±РѕСЂРѕС‚
                 parent[s2] = s1;
                 size[s1] += size[s2];
             }
@@ -45,16 +45,16 @@ public:
         this->V = V;
     }
     void symboltest(int x) {
-        if (x > 60 && x < 96) caster = 65; 
+        if (x > 60 && x < 96) caster = 65;
         else if (x > 96) caster = 97;
     }
 
     void addEdge(int x, int y, int w) {
         symboltest(x);
-        edgelist.push_back({ w,x-caster,y-caster});
+        edgelist.push_back({ w,x - caster,y - caster });
     }
     int kruskal() {
-        //тут сортировка, самый простой алгоритм
+        //С‚СѓС‚ СЃРѕСЂС‚РёСЂРѕРІРєР°, СЃР°РјС‹Р№ РїСЂРѕСЃС‚РѕР№ Р°Р»РіРѕСЂРёС‚Рј
         for (int i = 0; i < edgelist.size(); i++) {
             for (int j = i; j < (edgelist.size()); j++) {
                 if (edgelist[i] > edgelist[j]) {
@@ -69,14 +69,14 @@ public:
             int w = edge[0];
             int x = edge[1];
             int y = edge[2];
-            if (s.find(x) != s.find(y)) {//если две ноды не явлвяются частью одного поднможества, но могут быть
-                if (caster != 0) {//то выводим обе ноды
+            if (s.find(x) != s.find(y)) {//РµСЃР»Рё РґРІРµ РЅРѕРґС‹ РЅРµ СЏРІР»РІСЏСЋС‚СЃСЏ С‡Р°СЃС‚СЊСЋ РѕРґРЅРѕРіРѕ РїРѕРґРЅРјРѕР¶РµСЃС‚РІР°, РЅРѕ РјРѕРіСѓС‚ Р±С‹С‚СЊ
+                if (caster != 0) {//С‚Рѕ РІС‹РІРѕРґРёРј РѕР±Рµ РЅРѕРґС‹
                     int wa1 = (x + caster), wa2 = (y + caster);
                     cout << (static_cast<char>(wa1)) << " - " << (static_cast<char>(wa2)) << endl;
                 }
-                else cout << x << " - " << y << endl;//это на случай цифровых вершин
-                s.unite(x, y);//и склеиваем
-                ans += w;//увеличиваем вес дерева на вес 
+                else cout << x << " - " << y << endl;//СЌС‚Рѕ РЅР° СЃР»СѓС‡Р°Р№ С†РёС„СЂРѕРІС‹С… РІРµСЂС€РёРЅ
+                s.unite(x, y);//Рё СЃРєР»РµРёРІР°РµРј
+                ans += w;//СѓРІРµР»РёС‡РёРІР°РµРј РІРµСЃ РґРµСЂРµРІР° РЅР° РІРµСЃ 
             }
         }
         return ans;
@@ -91,7 +91,7 @@ int fileRoutine() {
         cout << "File not found\n";
         return 0;
     }
-    int y,count = 0;
+    int y, count = 0;
     char x;//change to int for digital verticles
     while (!File.eof()) {
         File >> x;
@@ -104,7 +104,7 @@ int fileRoutine() {
         count++;
     }
     File.close();
-    
+
     if (count < 51) return count;
     else {
         cout << "too big graph";
@@ -114,13 +114,13 @@ int fileRoutine() {
 
 int main()
 {
-    int w,q = fileRoutine();//тут проверяем файл
-    if (q == 0) return 7;//0 возвращается если с файлом что-то не так, тогда конец
-    Graph g(q); //создаётся граф
+    int w, q = fileRoutine();//С‚СѓС‚ РїСЂРѕРІРµСЂСЏРµРј С„Р°Р№Р»
+    if (q == 0) return 7;//0 РІРѕР·РІСЂР°С‰Р°РµС‚СЃСЏ РµСЃР»Рё СЃ С„Р°Р№Р»РѕРј С‡С‚Рѕ-С‚Рѕ РЅРµ С‚Р°Рє, С‚РѕРіРґР° РєРѕРЅРµС†
+    Graph g(q); //СЃРѕР·РґР°С‘С‚СЃСЏ РіСЂР°С„
     ifstream f;
     f.open("D:\\podelki\\aisd-kurs\\data2.txt");
-    char x, y;//для вершин описанных цифрами поменять char на int и убрать 1 у data
-    while (!f.eof()) {//читаем файл отсечками по 3 и добавляем в граф
+    char x, y;//РґР»СЏ РІРµСЂС€РёРЅ РѕРїРёСЃР°РЅРЅС‹С… С†РёС„СЂР°РјРё РїРѕРјРµРЅСЏС‚СЊ char РЅР° int Рё СѓР±СЂР°С‚СЊ 1 Сѓ data
+    while (!f.eof()) {//С‡РёС‚Р°РµРј С„Р°Р№Р» РѕС‚СЃРµС‡РєР°РјРё РїРѕ 3 Рё РґРѕР±Р°РІР»СЏРµРј РІ РіСЂР°С„
         f >> x;
         f >> y;
         f >> w;
@@ -128,69 +128,69 @@ int main()
     }
 
 
-   /* g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 11, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
-    g.addEdge(8, 9, 4);
-    g.addEdge(9, 10, 7);
-    g.addEdge(10, 2, 9);//17
-    g.addEdge(2, 11, 3);
-    g.addEdge(11, 7, 1);//19
-    g.addEdge(10, 11, 4);*/
-    //A=0, B=1, C=2, D=3, E=4, F=5, G=6, H=7, I=8, J=9, L=10, M=11
-    /*g.addEdge('A', 'B', 4);//для маленьких букв 65 заменить на 97
-    g.addEdge('A', 'H', 8);
-    g.addEdge('B', 'C', 8);
-    g.addEdge('B', 'H', 11);
-    g.addEdge('B', 'M', 7);
-    g.addEdge('C', 'I', 2);
-    g.addEdge('C', 'F', 4);
-    g.addEdge('D', 'E', 9);
-    g.addEdge('D', 'F', 14);
-    g.addEdge('E', 'F', 10);
-    g.addEdge('F', 'G', 2);
-    g.addEdge('G', 'H', 1);
-    g.addEdge('G', 'I', 6);
-    g.addEdge('H', 'I', 7);
-    g.addEdge('I', 'J', 4);
-    g.addEdge('J', 'L', 7);
-    g.addEdge('L', 'C', 9);//17
-    g.addEdge('C', 'M', 3);
-    g.addEdge('M', 'H', 1);//19
-    g.addEdge('L', 'M', 4);*/
-    
-    /*g.addEdge('a', 'b', 4);//для маленьких букв 65 заменить на 97
-    g.addEdge('a', 'h', 8);
-    g.addEdge('b', 'c', 8);
-    g.addEdge('b', 'h', 11);
-    g.addEdge('b', 'm', 7);
-    g.addEdge('c', 'i', 2);
-    g.addEdge('c', 'f', 4);
-    g.addEdge('d', 'e', 9);
-    g.addEdge('d', 'f', 14);
-    g.addEdge('e', 'f', 10);
-    g.addEdge('f', 'g', 2);
-    g.addEdge('g', 'h', 1);
-    g.addEdge('g', 'i', 6);
-    g.addEdge('h', 'i', 7);
-    g.addEdge('i', 'j', 4);
-    g.addEdge('j', 'l', 7);
-    g.addEdge('l', 'c', 9);//17
-    g.addEdge('c', 'm', 3);
-    g.addEdge('m', 'h', 1);//19
-    g.addEdge('l', 'm', 4);*/
-    
+    /* g.addEdge(0, 1, 4);
+     g.addEdge(0, 7, 8);
+     g.addEdge(1, 2, 8);
+     g.addEdge(1, 7, 11);
+     g.addEdge(2, 11, 7);
+     g.addEdge(2, 8, 2);
+     g.addEdge(2, 5, 4);
+     g.addEdge(3, 4, 9);
+     g.addEdge(3, 5, 14);
+     g.addEdge(4, 5, 10);
+     g.addEdge(5, 6, 2);
+     g.addEdge(6, 7, 1);
+     g.addEdge(6, 8, 6);
+     g.addEdge(7, 8, 7);
+     g.addEdge(8, 9, 4);
+     g.addEdge(9, 10, 7);
+     g.addEdge(10, 2, 9);//17
+     g.addEdge(2, 11, 3);
+     g.addEdge(11, 7, 1);//19
+     g.addEdge(10, 11, 4);*/
+     //A=0, B=1, C=2, D=3, E=4, F=5, G=6, H=7, I=8, J=9, L=10, M=11
+     /*g.addEdge('A', 'B', 4);//РґР»СЏ РјР°Р»РµРЅСЊРєРёС… Р±СѓРєРІ 65 Р·Р°РјРµРЅРёС‚СЊ РЅР° 97
+     g.addEdge('A', 'H', 8);
+     g.addEdge('B', 'C', 8);
+     g.addEdge('B', 'H', 11);
+     g.addEdge('B', 'M', 7);
+     g.addEdge('C', 'I', 2);
+     g.addEdge('C', 'F', 4);
+     g.addEdge('D', 'E', 9);
+     g.addEdge('D', 'F', 14);
+     g.addEdge('E', 'F', 10);
+     g.addEdge('F', 'G', 2);
+     g.addEdge('G', 'H', 1);
+     g.addEdge('G', 'I', 6);
+     g.addEdge('H', 'I', 7);
+     g.addEdge('I', 'J', 4);
+     g.addEdge('J', 'L', 7);
+     g.addEdge('L', 'C', 9);//17
+     g.addEdge('C', 'M', 3);
+     g.addEdge('M', 'H', 1);//19
+     g.addEdge('L', 'M', 4);*/
+
+     /*g.addEdge('a', 'b', 4);//РґР»СЏ РјР°Р»РµРЅСЊРєРёС… Р±СѓРєРІ 65 Р·Р°РјРµРЅРёС‚СЊ РЅР° 97
+     g.addEdge('a', 'h', 8);
+     g.addEdge('b', 'c', 8);
+     g.addEdge('b', 'h', 11);
+     g.addEdge('b', 'm', 7);
+     g.addEdge('c', 'i', 2);
+     g.addEdge('c', 'f', 4);
+     g.addEdge('d', 'e', 9);
+     g.addEdge('d', 'f', 14);
+     g.addEdge('e', 'f', 10);
+     g.addEdge('f', 'g', 2);
+     g.addEdge('g', 'h', 1);
+     g.addEdge('g', 'i', 6);
+     g.addEdge('h', 'i', 7);
+     g.addEdge('i', 'j', 4);
+     g.addEdge('j', 'l', 7);
+     g.addEdge('l', 'c', 9);//17
+     g.addEdge('c', 'm', 3);
+     g.addEdge('m', 'h', 1);//19
+     g.addEdge('l', 'm', 4);*/
+
 
     cout << g.kruskal();
     f.close();
